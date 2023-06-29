d9705e38e9edb35fbfb510b33f3985b899314be7
William Santana <williamandrade1058@gmail.com>
2023-06-29 11:23:53 -0300

    Create aula1.py

---
+++ projeto/secao3/aula1.py
@@ -0,0 +1,13 @@
+'''
+DocString
+Escrever o que eu quiser
+
+adsasdsadsa
+'''
+"""
+Usar para escrever suas notas
+"""
+# Permite escrever um comentário
+print("Hello, world!") # Na frente 
+print(123)
+# Abaixo
