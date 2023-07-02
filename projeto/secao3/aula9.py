a87d2d032d59ddf7bc3e4d80d54abf3106eadef2
William Santana <williamandrade1058@gmail.com>
2023-07-02 15:40:27 -0300

    Update aula9.py

---
+++ projeto/secao3/aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print(f"Adição: {adicao}")
+subtracao = 10 - 5
+print(f'Subtração: {subtracao}')
+multiplicacao = 10 * 10
+print(f"Multiplicação: {multiplicacao}")
+divisao = 10 / 2.2
+print(f"Divisão: {divisao:.2f}") # float
+divisao_inteira = 10 // 2.2
+print("Divisão inteira:", divisao_inteira)
+exponenciacao = 2 ** 10
+print("Exponenciação:", exponenciacao)
+modulo = 55 % 2 # resto da divisão
+print("Módulo:", modulo)
+
+print(10 & 8 == 0)
+print(16 & 8 == 0)
+print(10 & 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
