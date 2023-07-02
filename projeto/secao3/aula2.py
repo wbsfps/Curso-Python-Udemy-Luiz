7468fdc0a8aeaca3684e4ee52494e92e40a7525c
William Santana <williamandrade1058@gmail.com>
2023-07-02 12:45:31 -0300

    Update aula2.py

--- projeto/secao3/aula2.py
+++ projeto/secao3/aula2.py
@@ -1,4 +1,4 @@
 # \r \n -> CRLF
-print(12, 34, 1011, sep="/", end="##\n")  # Separador
+print(12, 34, 1011, sep="/", end="##\n")  # Separador / # No final do texto
 print(12, 34, sep="/", end="\n")
 print(56, 78, sep="/", end="\n")
