397c50a4cf348d81c5e575f0a944416574bd2fbc
William Santana <williamandrade1058@gmail.com>
2023-07-02 22:22:38 -0300

    Update aula15.py

---
+++ projeto/secao3/aula15.py
@@ -0,0 +1,12 @@
+# nome = input("Qual seu nome? ")
+numero_1 = (input("Digite um número: "))
+numero_2 = (input("Informe outro número: "))
+
+soma = numero_1 + numero_2
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f"A soma de {numero_1} + {numero_2} é: {soma}")
+# print(f"Seu nome é: {nome=}")
+# print(type(nome))
