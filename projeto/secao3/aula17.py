8f8bc2bee4ed3bedd246dec770a2dd6c2b4d9ffe
William Santana <williamandrade1058@gmail.com>
2023-07-03 10:13:45 -0300

    Update aula17.py

--- projeto/secao3/aula17.py
+++ projeto/secao3/aula17.py
@@ -1,12 +1,19 @@
 # if / elif    / else
 # se / se não se / se não
 
-entrada = input("Você quer entrar ou sair? ").strip().upper()
-if entrada == "entrar".upper().strip():
-    print("Você entrou no sistema!")
-elif entrada == "sair".upper().strip():
-    print("Você saiu do sistema")
-else:
-    print("Você não digitou nem entrar nem sair")
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
 
-print("Fora dos blocos")
+if condicao1:
+    print("Código para condição 1")
+    print("Código para condição 1")
+elif condicao2:
+    print("Código para condição 2")
+elif condicao3:
+    print("Código para condição 3")
+elif condicao4:
+    print("Código para condição 4")
+else:
+    print("Nenhuma condição foi satisfeita")
