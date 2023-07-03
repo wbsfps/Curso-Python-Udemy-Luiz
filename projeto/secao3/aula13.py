02790360d20303945d3286adf192044223cf408a
William Santana <williamandrade1058@gmail.com>
2023-07-02 21:54:44 -0300

    Update aula13.py

--- projeto/secao3/aula13.py
+++ projeto/secao3/aula13.py
@@ -1,5 +1,14 @@
 nome = str(input("Informe seu nome: "))
 altura = float(input("Informe sua altura: "))
-peso = float(input("Informe seu peso em quilogramas:"))
+peso = float(input("Informe seu peso em quilogramas: "))
 imc = peso / (altura ** 2)
-print(f"{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:1f}")
+
+"f-strings"
+linha_1 = f'{nome} tem {altura} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é:'
+linha_3 = f'{imc:.1f}'
+print(f"{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:,.1f}")
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
