20b2bb546b6d1e2e7ea9e23c387675ef3c541544
William Santana <williamandrade1058@gmail.com>
2023-07-03 21:04:06 -0300

    update

---
+++ projeto/secao3/aula20.py
@@ -0,0 +1,6 @@
+n1 = int(input("Informe um número: "))
+n2 = int(input("Informe outro número: "))
+if n1 > n2:
+    print(f"O primeiro valor é maior: {n1}")
+else:
+    print(f"O segundo valor é maior: {n2}")
