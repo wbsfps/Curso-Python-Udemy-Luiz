68de4a19a8888f934cc968c56d2b44f412ad60c1
William Santana <williamandrade1058@gmail.com>
2023-07-05 16:51:39 -0300

    update aula25

---
+++ projeto/secao3/aula25.py
@@ -0,0 +1,12 @@
+'''
+Interpolcação básica de strings
+s - string 
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+'''
+nome = "Luiz"
+preco = 1000.95897643
+variavel = "%s, o preço é R$%.2f" % (nome, preco)
+print(variavel)
+print('O hexademiral de %d é %04x' % (1500, 1500))
