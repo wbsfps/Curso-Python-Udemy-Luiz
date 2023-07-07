5c01f34223534d568ebbb4ded0675364c6f9e72f
William Santana <williamandrade1058@gmail.com>
2023-07-07 14:50:51 -0300

    update aula30

--- projeto/secao3/aula30.py
+++ projeto/secao3/aula30.py
@@ -4,22 +4,22 @@ Muitas condições no mesmo if (ruim)
    <- Contagem de complexidade (ruim)
 '''
 velocidade = 61 # Velocidade atual do carro
-localCarro = 101 # Local em que o carro está na estrada
+local_carro = 101 # Local em que o carro está na estrada
 
 RADAR_1 = 60 # Velocidade máxima do radar 1
 LOCAL_1 = 100 # Local onde o radar 1 está
 RADAR_RANGE = 1 # A distância onde o radar pega
 
-velocidadeCarroPassouRadar1 = velocidade > RADAR_1
+velocidade_carro_passou_radar1 = velocidade > RADAR_1
 
-carroPassouRadar1 = localCarro >= (LOCAL_1 - RADAR_RANGE) and localCarro <= (LOCAL_1 + RADAR_RANGE) 
+carro_passou_radar1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE) 
 
-carroMultadoRadar1 = velocidadeCarroPassouRadar1 and carroPassouRadar1
+carroMultadoRadar1 = velocidade_carro_passou_radar1 and carro_passou_radar1
 
-if velocidadeCarroPassouRadar1:
+if velocidade_carro_passou_radar1:
     print("Velocidade carro passou do radar 1")
 
-if carroPassouRadar1:
+if carro_passou_radar1:
     print("Carro passou do radar 1")
 
 if carroMultadoRadar1:
