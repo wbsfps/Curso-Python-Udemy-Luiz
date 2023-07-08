8118e719f1f640b24b3b51a360618a01f4b271ba
William Santana <williamandrade1058@gmail.com>
2023-07-08 13:07:04 -0300

    update aula32

--- projeto/secao3/aula32.py
+++ projeto/secao3/aula32.py
@@ -24,7 +24,7 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-# horario = (input("Que horas são? "))
+horario = (input("Que horas são? "))
 
 try:
     hora = int(horario) 
