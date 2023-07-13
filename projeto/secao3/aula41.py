0b5d3d471c3e9da0b495bba57e434719c0bc7966
William Santana <williamandrade1058@gmail.com>
2023-07-13 13:18:29 -0300

    update aula 41

---
+++ projeto/secao3/aula41.py
@@ -0,0 +1,13 @@
+'''while/else'''
+string = 'Valor qualquer'
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string')
