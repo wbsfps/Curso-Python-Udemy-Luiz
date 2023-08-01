c92ae0ddb02402e9bfea79f4fcc9e811a88e595d
William Santana <williamandrade1058@gmail.com>
2023-08-01 11:01:13 -0300

    atualizando arquivo da aula48

---
+++ projeto/secao3/aula48.py
@@ -0,0 +1,12 @@
+'''
+
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo 
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+'''
+
+#     01234
+#    -54321
+string = 'ABCDE' # 5 caracteres
