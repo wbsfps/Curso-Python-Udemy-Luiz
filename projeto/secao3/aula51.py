614908a9747e5269aaadb052cef57894d396ad32
William Santana <williamandrade1058@gmail.com>
2023-08-05 18:00:45 -0300

    arquivo aula 51 atualizado

---
+++ projeto/secao3/aula51.py
@@ -0,0 +1,7 @@
+"""
+Introdução ao desempacotamento + tuples (tuplas)
+"""
+# Quando tiver o underline (_) siginifica que a variável existe, mas não ela não vai ser utilizada
+_ ,nome, *_ = ['Maria', 'Helena', 'Luiz']
+print(nome)
+# No código acima está ignorando o primeiro e último e priorizando o valor do meio 
