7490f40f672d6071cbe871bb4ffb63f2e99104dd
William Santana <williamandrade1058@gmail.com>
2023-08-22 11:36:02 -0300

    arquivo testes atualizado

--- projeto/secao4/testes.py
+++ projeto/secao4/testes.py
@@ -34,28 +34,48 @@ print(listen(numbers=[1, 2, 4, 56, 6, 7, 32]))
 # Parâmetros e Retorno:
 # a) Crie uma função que receba dois números e retorne o maior deles.
 
+
 def biggest(n1, n2):
     if n1 > n2:
         return f'{n1}'
     return f'{n2}'
 
+
 print(biggest(4, 2))
 # b) Escreva uma função que receba uma lista de números e retorne a soma de todos os elementos.
-def sum_list (lista):
+
+
+def sum_list(lista):
     soma = sum(lista)
     return soma
-print(sum_list(lista = [1,2,3,4,5,6,7,8,9,10]))
+
+
+print(sum_list(lista=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 # c) Implemente uma função que aceite uma string e um número, e retorne a string repetida o número de vezes especificado.
+
+
 def string(string):
-    def multiply (number):
+    def multiply(number):
         return string * number
     return multiply
+
+
 multiplicador_string = string('a')
 print(multiplicador_string(10))
 
 # Questão 3
 # Funções Recursivas:
 # a) Defina uma função recursiva para calcular o fatorial de um número.
+
+
+def factorial(numero):
+    total = 1
+    for num in range(2, numero + 1):
+        total *= num
+    return total
+
+
+print(factorial(4))
 
 # b) Escreva uma função recursiva para calcular o N-ésimo termo da sequência de Fibonacci.
 # c) Implemente uma função recursiva para calcular o MDC (Máximo Divisor Comum) de dois números.
