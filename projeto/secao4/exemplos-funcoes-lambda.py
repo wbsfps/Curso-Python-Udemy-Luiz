812e508610720d9990f10bd52081d5c342acf6f5
William Santana <williamandrade1058@gmail.com>
2023-08-29 14:54:10 -0300

    arquivo exemplos de funcções lambda atualizado

---
+++ projeto/secao4/exemplos-funcoes-lambda.py
@@ -0,0 +1,27 @@
+def exec(function, *args):
+    return function(*args)
+
+
+def sum1(x, y):
+    return x+y
+
+
+def create_multiplier(multiplier):
+    def multiplied(number):
+        return number * multiplier
+    return multiplied
+
+
+# double = create_multiplier(2)
+double = exec(
+    lambda m: lambda n: n*m, 2
+)
+
+print(double(2))
+
+print(exec(lambda x, y: x+y, 2, 3),
+      exec(sum1, 2, 3),
+      sum1(2, 3)
+      )
+
+print(exec(lambda *args: sum(args), 1, 2, 32, 3, 23, 23, 2))
--- projeto/secao4/testes.py
+++ projeto/secao4/testes.py
@@ -98,38 +98,40 @@ persons = [
     }
 ]
 
-for name in persons:
-    print('Nome: ', name['name'])
+# for name in persons:
+#     print('Nome: ', name['name'])
 
-lista_de_listas_de_inteiros = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
-    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
-    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
-    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
-    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
-    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
-    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
-    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
-    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
-    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
-    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-]
-
-
-def find_first_duplicate(list_of_interable):
-
-    numbers_checks = set()
-    first_duplicate = -1
-
-    for number in list_of_interable:
-        if number in numbers_checks:
-            number = first_duplicate
-            break
-        numbers_checks.add(number)
-        print()
-    return first_duplicate
+# lista_de_listas_de_inteiros = [
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#     [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+#     [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+#     [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+#     [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+#     [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+#     [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+#     [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+#     [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+#     [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+#     [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+#     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+# ]
 
 
-for list in lista_de_listas_de_inteiros:
-    print(list, find_first_duplicate(list))
+# def find_first_duplicate(list_of_interable):
+
+#     numbers_checks = set()
+#     first_duplicate = -1
+
+#     for number in list_of_interable:
+#         if number in numbers_checks:
+#             number = first_duplicate
+#             break
+#         numbers_checks.add(number)
+#         print()
+#     return first_duplicate
+
+
+# for list in lista_de_listas_de_inteiros:
+#     print(list, find_first_duplicate(list))
+
+
