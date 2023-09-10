be6940896d111a25dad3978f1a42681a4669c156
William Santana <williamandrade1058@gmail.com>
2023-09-10 19:21:46 -0300

    arquivo filter_no_list_compre atualizado

---
+++ projeto/secao4/filter_no_list_compre.py
@@ -0,0 +1,33 @@
+import pprint
+
+
+def p(v):
+    pprint.pprint(news_products, sort_dicts=False, width=40)
+
+
+products = [
+    {'name': 'p1', 'price': 20, },
+    {'name': 'p2', 'price': 10, },
+    {'name': 'p3', 'price': 30, },
+]
+
+news_products = [
+    {**product, 'price': product['price'] * 1.05}
+    if product['price'] > 20 else {**product}
+    for product in products
+]
+
+# print(news_products)
+# print(*news_products, sep='\n')
+# p(news_products)
+
+# lista = [n for n in range(10) if n < 5]
+# print(lista)
+
+news_products = [
+    {**product, 'price': product['price'] * 1.05}
+    if product['price'] > 20 else {**product}
+    for product in products
+    if (product['price'] >= 20 and product['price'] * 1.05) > 10
+]
+p(news_products)
