12943454963dd07ebedab06437790600a0393d92
William Santana <williamandrade1058@gmail.com>
2023-09-15 12:28:38 -0300

    add arquivo dir_hasattr_getattr.py

--- projeto/secao4/dir_hasattr_getattr.py
+++ projeto/secao4/dir_hasattr_getattr.py
@@ -1,3 +1,14 @@
-# dir, hasattr e getattr em Python
+# dir - retorna todas as propriedades e métodos do objeto especificado.
+# hasattr - Sua principal tarefa é verificar se um objeto tem o atributo nomeado fornecido e retornar verdadeiro se presente, caso contrário, falso.
+# getattr - A função é usada para acessar o valor do atributo de um objeto e também dá a opção de executar o valor padrão em caso de indisponibilidade da chave.
+
 string = 'luiz'
-print(string.title())
+
+metodo = 'upper'
+
+if hasattr(string, metodo):  # ou if hasattr(string, 'upper')
+    print('Existe upper')
+    print(getattr(string, metodo)())
+    print(string)
+else:
+    print('Não existe o método', metodo)
