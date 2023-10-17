318e17339ab08f0b855be24284a95837b9966cce
William Santana <williamandrade1058@gmail.com>
2023-10-17 20:27:23 -0300

    add arquivos sobre init.py

---
+++ projeto/secao4/init.py
@@ -0,0 +1,5 @@
+import teste_packages
+
+print(teste_packages.sum1(1, 6))
+print(teste_packages.oi_speak())
+print(teste_packages.variable)
---
+++ projeto/secao4/teste_packages/__init__.py
@@ -0,0 +1,8 @@
+from teste_packages.modulo import sum1, variable, __all__
+from teste_packages.modulo2 import oi_speak
+
+# print('Você importou', __name__)
+
+
+# def dobra(x):
+#     return x * 2
