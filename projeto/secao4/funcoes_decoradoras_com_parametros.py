a4083603d89e8b2f26d36fc2014452a6149028ee
William Santana <williamandrade1058@gmail.com>
2025-03-02 12:04:09 -0300

    decoradores com parâmetros

---
+++ projeto/secao4/funcoes_decoradoras_com_parametros.py
@@ -0,0 +1,27 @@
+# Decoradoras com parâmetros
+
+def fabrica_de_decoradores(a=None, b=None, c=None):
+    def fabrica_de_funcoes(func):
+        print('Decoradora 1')
+
+        def aninhada(*args, **kwargs):
+            print('Aninhada')
+            res = func(*args, **kwargs)
+            return res
+
+        return aninhada
+
+    return fabrica_de_funcoes
+
+@fabrica_de_decoradores(1, 2, 3)
+def soma(a, b):
+    return a + b
+
+
+multiplica = fabrica_de_decoradores(1, 2, 3)(lambda x, y: x * y)
+
+dez_mais_cinco = soma(10, 5)
+dez_vezes_cinco = multiplica(10, 5)
+
+print(dez_mais_cinco)
+print(dez_vezes_cinco)
