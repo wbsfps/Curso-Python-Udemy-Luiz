c519ce64310adfc3b375835fa75ff73ccc1be080
William Santana <williamandrade1058@gmail.com>
2025-03-05 11:54:30 -0300

    revendo conceitos de copy

--- projeto/secao4/shallow_copy_deep_copy.py
+++ projeto/secao4/shallow_copy_deep_copy.py
@@ -17,9 +17,27 @@ d1 = {
     'l1': [0, 1, 2],
 }
 
-d2 = d1.copy()
-
+# Cópia rasa (shallow copy)
+d2 = copy.copy(d1)
 d2['c1'] = 1000
-d2['l1'][1] = 40
-print(d1)
-print(d2)
+d2['l1'][1] = 40  # Modifica a lista original
+print("Shallow Copy:")
+print("d1:", d1)
+print("d2:", d2)
+
+print()
+
+# Redefinir d1 para evitar influência da primeira modificação
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2],
+}
+
+# Cópia profunda (deep copy)
+d2 = copy.deepcopy(d1)
+d2['c1'] = 1000
+d2['l1'][1] = 40  # NÃO modifica a lista original
+print("Deep Copy:")
+print("d1:", d1)
+print("d2:", d2)
